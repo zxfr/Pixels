1ac31288f254e31c7e87bac9db3a7abcb65a1b48
zxfr <4@cyberbelarus.org>
2014-02-16 15:46:24 +0100

    Demo code adjusted to library structure changes

--- examples/PixelsTest/PixelsTest.ino
+++ examples/PixelsTest/PixelsTest.ino
@@ -1,12 +1,13 @@
 #include <Pixels_PPI16.h>
+#include <Pixels_Antialiasing.h>  // optional (a removal does not impact fonts antialiasing)
 #include <Pixels_HX8352.h>
 
 Pixels pxs(240, 400);
 
-
+//#include <Pixels_Antialiasing.h>
 //#include <Pixels_SPIsw.h>
 //#include <Pixels_ILI9341.h>
-//
+
 //Pixels pxs(240, 320);
 
 extern prog_uchar Eurostile13a[494] PROGMEM;
